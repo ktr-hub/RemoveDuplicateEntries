879a9c82b45832a13510e214123febd5e563bcad
KUMMARI TIRUPATHI RAO <35338095+ktr-hub@users.noreply.github.com>
2020-12-19 08:50:59 +0530

    [Tirupathi] Add. duplicate entries of dataTable removed

---
+++ AdoNet.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace adoDemo
+{
+    public class AdoNet
+    {
+        
+         public static string connectionString = @"Data Source=(LocalDb)\Ktr;Initial Catalog=AddressBookSystem;Integrated Security=True";
+         public static SqlConnection connection = new SqlConnection(connectionString);
+         
+
+        public static void selectUniqueRecords()
+        {
+            using (connection)
+            {
+                try
+                {
+                    string query = "select * from duplicateEntry";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    connection.Open();
+                    SqlDataAdapter sqlDataAdapter = new SqlDataAdapter();
+                    sqlDataAdapter.SelectCommand = command;
+                    
+                    DataSet ds = new DataSet();
+                    sqlDataAdapter.Fill(ds);
+                    sqlDataAdapter.Dispose();
+                    command.Dispose();
+
+                    
+                    for(int i=0;i<ds.Tables[0].Rows.Count;i++)
+                    {
+                        Console.WriteLine(ds.Tables[0].Rows[i].ItemArray[0]+" "+ ds.Tables[0].Rows[i].ItemArray[1]+" "+ ds.Tables[0].Rows[i].ItemArray[2]);
+                    }
+
+                    query = "delete from duplicateEntry";
+                    command = new SqlCommand(query, connection);
+                    int y = command.ExecuteNonQuery();
+                   
+
+                    Console.WriteLine("Unique");
+                    string[] columns = { "name", "address", "email" };
+                    DataTable dataTable = ds.Tables[0].DefaultView.ToTable(true,columns);
+
+                    for (int i = 0; i < dataTable.Rows.Count; i++)
+                    {
+                        Console.WriteLine(dataTable.Rows[i].ItemArray[0] + " " + dataTable.Rows[i].ItemArray[1] + " " + dataTable.Rows[i].ItemArray[2]);
+                        query = @"insert into duplicateEntry(name,address,email) values('"+dataTable.Rows[i].ItemArray[0].ToString()+"','"+dataTable.Rows[i].ItemArray[1].ToString()+"','"+dataTable.Rows[i].ItemArray[2].ToString()+"')";
+                        command = new SqlCommand(query, connection);
+                        int x= command.ExecuteNonQuery();
+                    }
+
+
+                    
+
+
+                }
+                catch(Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Linq;
+
+namespace adoDemo
+{
+
+    /*
+     public static string connectionString = @"Data Source=(LocalDb)\Ktr;Initial Catalog=payroll_service;Integrated Security=True";
+        SqlConnection connection = new SqlConnection(connectionString);
+
+        public void GetAllEmployee()
+        {
+            try
+            {
+                EmployeePayroll employee = new EmployeePayroll();
+                using (this.connection)
+                {
+                    string query = @"select empName,deptName,netPay,companyName from employee,department,employeeDepartment,payroll,company
+                                     where employeeDepartment.empId = employee.empId 
+                                     AND department.deptId = employeeDepartment.deptId
+                                     AND payroll.empId = employeeDepartment.empId;";
+
+                    SqlCommand cmd = new SqlCommand(query, this.connection);
+
+                    this.connection.Open();
+
+                    Console.WriteLine("\nConnection established with the database");
+
+                    SqlDataReader dr = cmd.ExecuteReader();
+
+                    if (dr.HasRows)
+                    {
+                        Console.WriteLine("\nReading Data Rows : ");
+                        while (dr.Read())
+                        {
+                            employee.EmployeeName = dr.GetString(0);
+                            employee.Department = dr.GetString(1);
+                            employee.Country = dr.GetString(3);
+                            Console.WriteLine("\n"+employee.EmployeeName + " " + employee.Department + " " + employee.Country);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No data");
+                    }
+                    dr.Close();
+
+                    this.connection.Close();
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                this.connection.Close();
+            }
+        }
+     
+     
+     */
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sql Remove Duplicate entry!");
+            //duplicate();
+            AdoNet.selectUniqueRecords();
+        }
+
+        public static void duplicate()
+        {
+            int[] arr = { 1, 1, 1, 2, 2, 2, 3, 3, 4, 4, 5 };
+            arr = arr.Distinct().ToArray();
+            Array.ForEach(arr, x => 
+            { 
+                Console.WriteLine(x);
+            });
+        }
+    }
+}
